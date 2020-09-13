de99380b1931620ceff6b53a680bf7c4aea08398
Tim Kmecl <tim.kmecl.tk@gmail.com>
2020-09-13 21:59:32 +0200

    Updated mandelbrot.py for animation

--- Buddhabrot/mandelbrot.py
+++ Buddhabrot/mandelbrot.py
@@ -4,75 +4,122 @@ import math
 import matplotlib.pyplot as plt
 import random
 from tqdm import tqdm
+import os
 
 
-res = 1080
+''' Parameters '''
+
+# Range on Re and Im axes
 view = np.array([[-2, 1], [-1.5, 1.5]])
+
+# Iterated function
 
 
 def function(z, c):
     return z**2 + c
 
 
-def recursive(f, n):
-    def f1(x, *args):
-        for i in range(n):
-            x = f(x, *args)
-        return x
-    return f1
+# Initital sequence element
+z0 = -0.7+0.5j
+
+# Number of trajectories and iterations
+pts = 1_000_000
+its = 5_000
 
 
-l = []
-density = np.zeros((res, res), dtype=np.float32)
+''' Code '''
+
+# Generates inital points for trajectories
 
 
-cc = 0
-pts = 10_000
-its = 1500
-for i in tqdm(range(pts)):
-    z = np.random.rand(2) * (view[:, 1] - view[:, 0]) + view[:, 0]
-    z = z[0] + 1j*z[1]
-    c = z
-    z = function(0, c)
+def inital_points(pts, view):
+    z1 = np.random.rand(pts, 2) * (view[:, 1] - view[:, 0]) + view[:, 0]
+    z1 = z1[:, 0] + 1j*z1[:, 1]
+    return z1
+
+
+# Calculates trajectories for inital points
+def calculate_trajectories(z0, z1, func, pts, its, remove_cardioid=False):
+    c = np.copy(z1)
+    if remove_cardioid:
+        x = np.logical_not(
+            (np.abs(c)**2) * (8 * np.abs(c)**2 - 3) <= 3/32 - c.real)
+        c = c[x]
+
+    z = np.full_like(c, z0)
+    ind = np.arange(z.shape[0])
 
     n = 0
     l1 = []
-    while np.linalg.norm(z) < 4 and n < its:
-        l1.append(np.array((z.real, z.imag, n)))
+
+    for i in tqdm(range(its)):
         z = function(z, c)
         n += 1
-        cc += 1
-    if np.linalg.norm(z) < 4:
-        l.extend(l1)
-print(cc, cc/(pts*its))
+        l1.append([z.copy(), ind.copy(), n])
+
+        x = np.abs(z) < 2
+        c = c[x]
+        z = z[x]
+        ind = ind[x]
+
+    return l1, ind
 
 
-for z in tqdm(l):
-    zz = z[0:2]
-    it = z[2]
-    n = res * (zz - view[:, 0]) / (view[:, 1] - view[:, 0])
+# Generates a list of coordinates from previous function's output
+def change_data_shape(l1, ind):
+    l = []
+    ll = []
+    for a in tqdm(l1[1:]):
+        x = np.logical_not(np.isin(a[1], ind))
+        b = a[0][x]
+        #l2 = np.stack((a[0][x].real, a[0][x].imag, np.full(a[0][x].shape, a[2]))).T
+        # l.extend(l2.tolist())
+        l2 = [[b[i].real, b[i].imag, a[2]] for i in range(b.shape[0])]
+        l.extend(l2)
 
-    if (n >= res - 1).any() or (n < 0).any():
-        continue
+    ll = [[e[0] for e in l], [e[1] for e in l], [e[2] for e in l]]
 
-    x = math.floor(n[0])
-    y = math.floor(n[1])
+    return ll
 
-    if True:
-        #density[x, y] += 1*math.sqrt(it)
-        density[x, y] += 1
+
+# Generates a 2d histogram of points and point*current_iteration
+def to_histogram(res, ll):
+    H = np.histogram2d(ll[0], ll[1], bins=res, range=view)
+    density = H[0]
+    H2 = np.histogram2d(ll[0], ll[1], bins=res, range=view, weights=ll[2])
+    density2 = H2[0]
+
+    return density, density2
+
+
+# Draws an image
+def draw(density, density2, divide_by=1, show_2=True, vmax=1, show=True, save=False, filename="ttt.png"):
+    if show_2:
+        final = density2.copy()/divide_by
     else:
-        xF = float(n[0]) % 1.0
-        yF = float(n[1]) % 1.0
-        density[x, y] += (xF + yF)/4
-        density[x + 1, y] += (1 - xF + yF)/4
-        density[x, y + 1] += (xF + 1 - yF)/4
-        density[x + 1, y + 1] += (2 - xF - yF)/4
+        final = density.copy()/divide_by
 
-final = density.copy()/50
-final = np.sqrt(density)
+    final = np.sqrt(final)
 
-vmax = 5
-plt.imshow(final, cmap='Greys_r', vmin=0, vmax=vmax)
-plt.show()
-plt.imsave('t7.png', final, cmap='Greys_r', vmin=0, vmax=vmax)
+    if show:
+        plt.imshow(final, cmap='Greys_r', vmin=0, vmax=vmax)
+        plt.show()
+    if save:
+        plt.imsave(os.path.join(os.path.dirname(__file__), 'img', filename), final, cmap='Greys_r', vmin=0, vmax=vmax)
+
+
+def main():
+    z1 = inital_points(pts, view)
+    r = 30
+    for i in range(r):
+        print(i)
+        phi = (math.pi * i/r) / 10
+        z0 = (math.cos(phi) + 1j * math.sin(phi)) * 1
+        l = change_data_shape(*calculate_trajectories(z0, z1, function, pts, its))
+        hist = to_histogram(1080, l)
+        draw(*hist, 35, show=False, show_2=False, save=True, filename=f'an2/a{i}.png')
+        draw(*hist, 7000, show=False, save=True, filename=f'an1/it{i}.png')
+
+
+if __name__ == '__main__':
+    main()
